9225c17104d86da400233a6b88adc3cdf1f35525
Eugen Martin <eugeniusmartinus@gmail.com>
2015-07-28 18:23:57 +0200

    add REDISTOGO_URL

--- lita_config.rb
+++ lita_config.rb
@@ -9,7 +9,7 @@ Lita.configure do |config|
   config.robot.adapter = :slack
   config.adapters.slack.token = "xoxb-8307047347-bxY9xMxv7vaFH9KYVbp8DwZA"
 
-  # config.redis[:url] = ENV["REDISTOGO_URL"]
+  config.redis[:url] = ENV["REDISTOGO_URL"]
   config.http.port = ENV["PORT"]
   config.handlers.samantha.db_url = ENV['SAMANTHA_DB_URL']
 end
